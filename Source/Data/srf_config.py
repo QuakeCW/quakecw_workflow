98900eba4792bee99e241d2707e6b37139d115ee
Sung Bae <sung.bae@canterbury.ac.nz>
2022-04-22 07:29:53 +0900

    Implemented make_vm

--- Source/Data/srf_config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# default 1d velocity model
-#VELOCITY_MODEL = "%s/lp_generic1d-gp01_v1.vmod" % (script_dir)
-VELOCITY_MODEL= "/home01/x2319a02/gmsim/VelocityModel/Mod-1D/kr_gb_kim2011_modified.1d"
