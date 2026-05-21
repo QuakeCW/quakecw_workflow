fad55cc4b1e7a9bc46e123a0e890dee30d4ec389
Sung Bae <sung.bae@canterbury.ac.nz>
2026-05-21 12:32:43 +0900

    Fixed to use the latest visualisation release

--- install.sh
+++ install.sh
@@ -290,7 +290,7 @@ if checkpoint_exists "STEP6_QUAKECW_PACKAGES"; then
         uv pip install "${GIT_BASE}/qcore.git@${RELEASE}.1"
         uv pip install --no-build-isolation "${GIT_BASE}/IM_calculation.git@${RELEASE}"
         uv pip install --no-build-isolation "${GIT_BASE}/Pre-processing.git@${RELEASE}"
-        uv pip install --no-build-isolation "${GIT_BASE}/visualisation.git@${RELEASE}"
+        uv pip install --no-build-isolation "${GIT_BASE}/visualisation.git@${RELEASE}.1"
         uv pip install --no-build-isolation "${GIT_BASE}/slurm_gm_workflow.git@${RELEASE}"
         mark_checkpoint "STEP6_QUAKECW_PACKAGES"
     fi
@@ -298,7 +298,7 @@ else
     uv pip install "${GIT_BASE}/qcore.git@${RELEASE}.1"
     uv pip install --no-build-isolation "${GIT_BASE}/IM_calculation.git@${RELEASE}"
     uv pip install --no-build-isolation "${GIT_BASE}/Pre-processing.git@${RELEASE}"
-    uv pip install --no-build-isolation "${GIT_BASE}/visualisation.git@${RELEASE}"
+    uv pip install --no-build-isolation "${GIT_BASE}/visualisation.git@${RELEASE}.1"
     uv pip install --no-build-isolation "${GIT_BASE}/slurm_gm_workflow.git@${RELEASE}"
     mark_checkpoint "STEP6_QUAKECW_PACKAGES"
 fi
